b59039e592eabd4f04cd3529c4edb8dc1568f8de
Cheryet <C.heryet@outlook.com>
2022-11-08 13:29:46 -0700

    implemented character counter for tweet length

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,19 @@
 
 
-
-
 $(document).ready(function() {
   $("textarea[name='text']").on('input', function() {
-    
+    let total = 140;
+    let length = $( this ).val().length;
+    let count = total - length;
+    $('output[name="counter"]').text(count)
+
+    if (count < 0 ) {
+      $('output[name="counter"]').css({'color': 'red'})
+    } else {
+      $('output[name="counter"]').css({'color': 'black'})
+
+    }
   })
+
   
 });
