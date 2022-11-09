f4bb792bee4a29373ba30b915caa763dd3a67c43
Cheryet <C.heryet@outlook.com>
2022-11-09 09:23:10 -0700

    implmented renderTweets function to render all tweets from a Json object

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -31,25 +31,41 @@ $(document).ready(function() {
     return $tweet;
   }
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+  const renderTweets = function (array) {
+    
+    for (let obj of array) {
+      const $tweet = createTweetElement(obj);
+      $('.tweet-feed').append($tweet);
+    }
+
+  }
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
         "handle": "@SirIsaac"
       },
-    "content": {
+      "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-    "created_at": 1461116232227
-  }
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
   
-  const $tweet = createTweetElement(tweetData);
-  
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('.tweet-feed').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
+  renderTweets(data);
 })
 
 
