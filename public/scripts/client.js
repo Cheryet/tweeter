9008789c18fc4f8d9f1e9fa126667df535247aef
Cheryet <C.heryet@outlook.com>
2022-11-09 13:55:28 -0700

    implemented get request from /tweets to get json object

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -61,42 +61,25 @@ $(document).ready(function() {
     $.post( "/tweets", tweetForm.serialize() );
   
   }
+
+  const loadTweets = () => {
+    $.get('/tweets', function(data) {
+      console.log('Data Type: ' ,typeof data)
+      console.log(data)
+      renderTweets(data)
+    })
+
+    
+    
+    
+    
+  }
   
+  loadTweets()
 
   tweetForm.submit(handleTweetForm)
   
-  
-  
-
-
-  
-
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-  
-  renderTweets(data);
+  //renderTweets(data);
 })
 
 
