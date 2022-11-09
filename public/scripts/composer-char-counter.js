833371854e05140afa2bbd131345687eea8fe15f
Cheryet <C.heryet@outlook.com>
2022-11-09 13:30:31 -0700

    added comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,14 @@
 
 
 $(document).ready(function() {
+  //Counts total input on text area and pushes to page
   $("textarea[name='text']").on('input', function() {
     let total = 140;
     let length = $( this ).val().length;
     let count = total - length;
     $('output[name="counter"]').text(count)
 
+    //adjusts colour of counter
     if (count < 0 ) {
       $('output[name="counter"]').css({'color': 'red'})
     } else {
