d6cf37979d164e5439993bc11a95fc3aa4b2f1be
Cheryet <C.heryet@outlook.com>
2022-11-10 15:08:11 -0700

    linted file

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,23 +1,19 @@
 
 
 $(document).ready(function() {
-  //Counts total input on text area and pushes to page
+  //Counts total inputs in text area and pushes to page
   $("textarea[name='text']").on('input', function() {
     let total = 140;
-    let length = $( this ).val().length;
+    let length = $(this).val().length;
     let count = total - length;
-    $('output[name="counter"]').text(count)
-
-    
+    $('output[name="counter"]').text(count);
 
     //adjusts colour of counter
-    if (count < 0 ) {
-      $('output[name="counter"]').css({'color': 'red'})
+    if (count < 0) {
+      $('output[name="counter"]').css({'color': 'red'});
     } else {
-      $('output[name="counter"]').css({'color': 'black'})
-
+      $('output[name="counter"]').css({'color': 'black'});
     }
-  })
-
-  
+    
+  });
 });
